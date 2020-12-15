e7e676a44e5f839678ee23d2998716169845d9a3
Juha Yrjölä <juha.yrjola@iki.fi>
2020-12-15 21:08:08 +0200

    Hopefully supports CORS cookies

--- graphql_backend.py
+++ graphql_backend.py
@@ -2,13 +2,12 @@ from flask import Flask
 from flask_babel import Babel
 from flask_cors import CORS
 from flask_session import Session
-from graphql_server.flask import GraphQLView
-
 from graphql_schema import schema
+from graphql_server.flask import GraphQLView
 
 app = Flask(__name__)
 
-CORS(app)  # Enable Cross-Origin headers
+CORS(app, supports_credentials=True, origins=['*'])  # Enable Cross-Origin headers
 
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view(
     'graphql',
