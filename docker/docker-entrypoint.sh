d79c9353def21a685cc39f90ae28c2caef012b2a
Juha Yrjölä <juha.yrjola@iki.fi>
2021-01-13 18:19:43 +0200

    Update Google mobility data before starting graphql backend

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,5 +2,8 @@
 
 cd /app
 
+# Download updated mobility dataset
+python -m data_import.google_covid_mobility
+
 # Log to stdout
 exec gunicorn --access-logfile - -R -w 4 --bind 0.0.0.0:5000 graphql_backend:app 
